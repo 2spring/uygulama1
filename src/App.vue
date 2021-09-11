a514ccc6c0d29d1d7fc3fd660f05482261e33886
denemeisimi <denemeamacli@test.com>
2021-09-11 20:27:01 +0200

    12 changes deneme1

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Uygulamaya Hosgeldiniz"/>
+  <bilesen1 class="ortala" @click="CartaBas" />
+
+  <h2>Adamim ya nasilsin</h2>
+</template>
+
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue' ;
+import bilesen1 from './components/bilesen1.vue'
+
+export default {
+  name: 'App',
+  components: { HelloWorld, bilesen1 },
+  methods: {
+    CartaBas()  {
+      alert("Karta Bastin!");
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #20b81b;
+  margin-top: 60px;
+  background-color: black;
+}
+</style>
---
+++ src/components/bilesen1.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="card">
+Card Content
+
+</div>
+
+  
+</template>
+
+<script>
+export default {
+    name: 'bilesen1'
+    
+
+}
+</script>
+
+<style scoped>
+
+
+.card {
+ box-shadow: 0 4px 8px 0 rgba(199, 38, 38, 0.5);
+ transition: 0.3s;
+ padding: 16px;
+ margin-bottom: 20px;
+ width: 200px;
+ background-color: rgb(46, 24, 6);
+
+}
+
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(201, 183, 183, 0.5)
+}
+
+.ortala {  
+    margin-left: 30%;
+}
+
+
+</style>
+
